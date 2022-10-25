859158c01eb7f0cb35c9b8ebebca7e6232e88f29
aliakseiv <1189185@gmail.com>
2022-10-25 20:36:13 +0200

    construction

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    double value1;
+    double value2;
+
+    public Calculator(double value1, double value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+    public void sum(){
+        System.out.println(value1+value2);
+    }
+    public void differnce(){
+        System.out.println(value1-value2);
+    }
+    public void multiplicate(){
+        System.out.println(value1*value2);
+    }
+    public void division(){
+        System.out.println(value1/value2);
+    }
+}
